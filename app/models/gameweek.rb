eec74d9eecf6b14fed001128c75d9d49646eaa2f
John Paul <john.mcfeely@invisionwfm.com>
2012-08-04 09:53:00 +0100

    Changed CSS for application header

--- app/models/gameweek.rb
+++ app/models/gameweek.rb
@@ -2,4 +2,5 @@ class Gameweek < ActiveRecord::Base
   attr_accessible :start_date, :start_time, :number_of_fixtures, :fixtures_attributes, :prediction_attributes
   has_many :fixtures, :dependent => :destroy
   accepts_nested_attributes_for :fixtures
+  belongs_to :user
 end
--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,3 +1,4 @@
 class Prediction < ActiveRecord::Base
   attr_accessible :away_goals, :fixture_id, :home_goals, :user_id
+  belongs_to :fixtures
 end
